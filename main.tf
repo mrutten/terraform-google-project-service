273d13a59b12545882c814c1ae70dcd11cf282e9
Mark Rutten <mark.rutten@binaryvision.nl>
2023-10-19 19:22:07 +0200

    Changed services to set

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
   APIs configuration
  *****************************************/
 resource "google_project_service" "project_services" {
-  for_each                   = var.services
+  for_each                   = toset(var.services)
   project                    = var.project_id
   service                    = each.value
   disable_on_destroy         = var.disable_services_on_destroy
